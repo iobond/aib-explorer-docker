f2baa699270875278ce4538adb39cac7d23ea5d8
AIB Core Development <aib@iobond.com>
2017-12-16 12:37:11 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
-wget https://github.com/iobond/aib-explorer-docker/releases/download/aib.blockdata/aib.blockdata.tgz
+wget https://github.com/iobond/aib-explorer-docker/releases/download/aib-1217295/aib-1217295.tgz
 mkdir -p /home/block.aib.one
-tar xf http://tk.9mmo.com/dl/aib.blockdata.tgz -C /home/block.aib.one/
-#docker run -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  -p 33080:80 -p 33022:22 --name block.AIB.one  canadianbitcoin/block.aib.one:v3 /root/start.sh
-docker run -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  --name block.AIB.one  canadianbitcoin/block.aib.one:v3 /root/start.sh
+tar xf http://tk.9mmo.com/dl/aib-1217295.tgz -C /home/block.aib.one/
+docker run -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  -p 33080:80 -p 33022:22 --name block.AIB.one  canadianbitcoin/block.aib.one:v3 /root/start.sh
+#docker run -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  --name block.AIB.one  canadianbitcoin/block.aib.one:v3 /root/start.sh
