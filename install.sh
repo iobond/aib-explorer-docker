1e54d927697c24e60b43369c96a3d6251b83616e
9CAT <9cat@users.noreply.github.com>
2017-12-21 16:26:39 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aib-12
 mkdir -p /home/block.aib.one
 tar xf /root/TRASH/aib-1219052.tgz -C /home/block.aib.one/
 curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aibd-v5.13.3.3/aibd.tgz -o /root/TRASH/aibd.tgz 
+rm -rf /home/block.aib.one/.aib/blocks
+rm -rf /home/block.aib.one/.aib/chainstate
 tar xf /root/TRASH/aibd.tgz -C /home/block.aib.one/.aib/
+rm /home/block.aib.one/.aib/*.log
 
-#docker run --restart=always -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  -p 33080:80 -p 33022:22 --name block.AIB.one  canadianbitcoin/block.aib.one:v6 /root/start.sh
-docker run --restart=always -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  --name block.AIB.one  canadianbitcoin/block.aib.one:v6 /root/start.sh
+docker run --restart=always -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  -p 33080:80 -p 33022:22 --name block.AIB.one  canadianbitcoin/block.aib.one:v6 /root/start.sh
+#docker run --restart=always -v /home/block.aib.one:/home/.aib  -v /home/.logs:/root/.logs -d -p 31415:31415  -p 33001:3001  --name block.AIB.one  canadianbitcoin/block.aib.one:v6 /root/start.sh
