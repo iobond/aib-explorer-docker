a0ef9b57c2dfe4b04a297d877106be09e650723a
AIB Core Development <aib@iobond.com>
2017-12-21 16:29:40 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 mkdir -p /root/TRASH
 curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aib-1219052/aib-1219052.tgz -o /root/TRASH/aib-1219052.tgz
 mkdir -p /home/block.aib.one
-tar xf /root/TRASH/aib-1219052.tgz -C /home/block.aib.one/
-curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aibd-v5.13.3.3/aibd.tgz -o /root/TRASH/aibd.tgz 
 rm -rf /home/block.aib.one/.aib/blocks
 rm -rf /home/block.aib.one/.aib/chainstate
+tar xf /root/TRASH/aib-1219052.tgz -C /home/block.aib.one/
+curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aibd-v5.13.3.3/aibd.tgz -o /root/TRASH/aibd.tgz 
+
 tar xf /root/TRASH/aibd.tgz -C /home/block.aib.one/.aib/
 rm /home/block.aib.one/.aib/*.log
 
